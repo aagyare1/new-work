4da237d84bfb3effc86bffee5a99a09cd86a6ea2
Aretha Agyare <63205806+aagyare1@users.noreply.github.com>
2021-02-18 14:39:34 -0600

    Add files via upload

---
+++ mathcheck.py
@@ -0,0 +1,77 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "greek-brain",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter the first num: 4\n",
+      "Enter the second num: 3\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "4.0 + 3.0 = 7.0\n",
+      "4.0 - 3.0 = 1.0\n",
+      "4.0 * b'=' 12.0\n",
+      "4.0 / 3.0 = 1.3333333333333333\n",
+      "4.0 % 3.0 = 1.0\n",
+      "4.0 ^ 3.0 = 64.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=float(input(\"Enter the first num:\"))\n",
+    "b=float(input (\"Enter the second num:\"))\n",
+    "c=a+b\n",
+    "print(a, '+',b,\"=\",c)\n",
+    "c=a-b\n",
+    "print (a,'-',b,'=',c)\n",
+    "c=a*b\n",
+    "print (a,'*',b'=',c)\n",
+    "c=a/b\n",
+    "print ( a,'/',b,'=',c)\n",
+    "c=a%b\n",
+    "print(a,'%',b,\"=\",c)\n",
+    "c=a**b\n",
+    "print(a,'^',b,'=',c)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "sound-discretion",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ mergetown.py
@@ -0,0 +1,78 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "duplicate-softball",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "What town are you from? arlington\n",
+      "In what state were you born? texas\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "So you are from arlington,texas\n"
+     ]
+    }
+   ],
+   "source": [
+    "town= input(\"What town are you from?\")\n",
+    "state=input(\"In what state were you born?\")\n",
+    "fullLocation= town +','+ state\n",
+    "print(\"So you are from\", fullLocation)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "vital-struggle",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "accredited-copying",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "conditional-athletics",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
